d7990a0f1543f4402d9ff03f005101daa03ee707
Luiz Fernando Jr <junior_lfs@hotmail.com>
2023-10-27 00:51:36 -0300

    correct image description

--- script.js
+++ script.js
@@ -7,8 +7,16 @@ function toggleMode() {
   if (html.classList.contains("light")) {
     //se tiver light mode nova imagem
     img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute(
+      "alt",
+      "Foto de Mayke Brito sorrindo, usando óculos escuros, jaqueta preta e fundo degradê do rosa para o azul"
+    )
   } else {
     // se não manter a imagem original
     img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute(
+      "alt",
+      "Foto de Mayke Brito sorrindo, usando óculos e camisa preta, barba e fundo amarelo"
+    )
   }
 }
